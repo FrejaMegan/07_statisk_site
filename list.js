76381dc9ec1baeee2bbfa44613be961ed0ca29dd
Freja Megan Lindahl-Dunton <frejamegan@hotmail.com>
2025-02-03 18:38:56 +0100

    add liste med de produkter i den kategori der blev klikket på

--- list.js
+++ list.js
@@ -1,13 +1,16 @@
+const myCategory = new URLSearchParams(window.location.search).get("category");
 const productContainer = document.querySelector(".product_list_container");
 
-fetch("https://kea-alt-del.dk/t7/api/products?limit=100")
+fetch(`https://kea-alt-del.dk/t7/api/products?${myCategory}`)
   .then((response) => response.json())
   .then((products) => showList(products));
 
 function showList(products) {
   console.log("Fetched products:", products);
 
-  const markup = products.map((product) => `
+  const markup = products
+    .map(
+      (product) => `
       <div class="product_card"> 
         <a href="produkt.html">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}">
@@ -16,8 +19,11 @@ function showList(products) {
 
         <p class="product_price">${product.price} DKK</p>
       </div>
-  `).join(""); 
-
-  productContainer.innerHTML = markup;
+  `
+    )
+    .join("");
+    console.log(markup);
+    productContainer.innerHTML = markup;
 }
+
 
